508566de6026c4c190072ebd1f726ff467967348
johnnyjr <johnnyjr@tomcats.se>
2018-05-22 13:16:32 +0200

    Sound is working

---
+++ Assets/PlayVideo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(VideoPlayer), typeof(AudioSource))]
+public class PlayVideo : MonoBehaviour {
+
+    private VideoPlayer videoPlayer;
+    private VideoSource videoSource;
+    private AudioSource audioSource;
+
+    void Start () {
+        videoPlayer = GetComponent<VideoPlayer>();
+        audioSource = GetComponent<AudioSource>();
+
+        videoPlayer.playOnAwake = false;
+        audioSource.playOnAwake = false;
+
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+
+        videoPlayer.EnableAudioTrack(0, true);
+        videoPlayer.SetTargetAudioSource(0, audioSource);
+
+        videoPlayer.prepareCompleted += StartVideo;
+        videoPlayer.Prepare();
+
+    }
+
+    private void StartVideo(VideoPlayer source)
+    {
+        source.Play();
+    }
+}
